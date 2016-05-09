14b8b0d93988ce647809fcd880f445773c672ce7
Daniel Wulff <danielwulff.ee@gmail.com>
2016-05-09 14:47:07 -0700

    created the required interface tests, many failing

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -27,7 +27,8 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("Hair Salon");
   }
 
-  @Test public void createStylistAndViewTest() {
+  //create a stylist and view list of stylists
+  @Test public void createStylistAndViewList() {
     goTo("http://localhost:4567/");
     click("a", withText("Create New Stylist"));
     fill("#name").with("Stephen");
@@ -35,7 +36,8 @@ public class AppTest extends FluentTest {
     assertThat(pageSource().contains("Stephen"));
   }
 
-  @Test public void viewStylistPage() {
+  //view individual stylist page
+  @Test public void viewAStylistPage() {
     goTo("http://localhost:4567/");
     click("a", withText("Create New Stylist"));
     fill("#name").with("Stephen");
@@ -44,7 +46,32 @@ public class AppTest extends FluentTest {
     assertThat(pageSource().contains("Stylist Stephen's clients:"));
   }
 
-  @Test public void createClientForStylistAndView() {
+  //edit stylist name
+  @Test public void editAStylistName() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Create New Stylist"));
+    fill("#name").with("Stephen");
+    submit(".btn");
+    click("a", withText("Stephen"));
+    click("a", withText("Change stylist name"));
+    fill("#name").with("Sarah");
+    submit(".btn");
+    assertThat(pageSource().contains("Sarah"));
+  }
+
+  //delete stylist
+  @Test public void deleteAStylist() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Create New Stylist"));
+    fill("#name").with("Stephen");
+    submit(".btn");
+    click("a", withText("Stephen"));
+    click("a", withText("Delete Stylist"));
+    assertEquals(0, Stylist.all().size());
+  }
+
+  //add a client to an individual stylist
+  @Test public void createClientForStylistAndViewList() {
     goTo("http://localhost:4567/");
     click("a", withText("Create New Stylist"));
     fill("#name").with("Stephen");
@@ -54,5 +81,51 @@ public class AppTest extends FluentTest {
     fill("#name").with("Stacy");
     submit(".btn");
     assertThat(pageSource().contains("Stacy"));
+  }
+
+  //view individual client page
+  @Test public void viewClientPage() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Create New Stylist"));
+    fill("#name").with("Stephen");
+    submit(".btn");
+    click("a", withText("Stephen"));
+    click("a", withText("Create client"));
+    fill("#name").with("Stacy");
+    submit(".btn");
+    click("a", withText("Stacy"));
+    assertThat(pageSource().contains("Client: Stacy"));
+  }
+
+  //edit a client name
+  @Test public void editClientAndView() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Create New Stylist"));
+    fill("#name").with("Stephen");
+    submit(".btn");
+    click("a", withText("Stephen"));
+    click("a", withText("Create client"));
+    fill("#name").with("Stacy");
+    submit(".btn");
+    click("a", withText("Stacy"));
+    click("a", withText("Change client name"));
+    fill("#name").with("Shawn");
+    submit(".btn");
+    assertThat(pageSource().contains("Shawn"));
+  }
+
+  //delete a client
+  @Test public void deleteAClientAnd() {
+    goTo("http://localhost:4567/");
+    Stylist myStylist = new Stylist("Stephen");
+    myStylist.save();
+    goTo("http://localhost:4567/stylists/");
+    click("a", withText("Stephen"));
+    click("a", withText("Create client"));
+    fill("#name").with("Stacy");
+    submit(".btn");
+    click("a", withText("Stacy"));
+    click("a", withText("Delete Client"));
+    assertEquals(0, myStylist.getClients().size());
   }
 }
