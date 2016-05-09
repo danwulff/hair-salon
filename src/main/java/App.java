59b8f19fcd240767aacb72a5309303fe39a4df7a
Daniel Wulff <danielwulff.ee@gmail.com>
2016-05-09 16:50:24 -0700

    finished fixing client interface tests, client interface logic working and all interface tests now passing. Todo: add readme, view webpages and fix typos/change styling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -86,5 +86,56 @@ public class App {
       response.redirect("/stylists/");
       return null;
     });
+
+    //page for client creation format
+    get("/stylists/:id/clients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("template", "templates/client-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //create new client object
+    post("/stylists/:id/clients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int stylistId = Integer.parseInt(request.queryParams("stylist_id"));
+      String clientName = request.queryParams("name");
+
+      Client myClient = new Client(clientName, stylistId);
+      myClient.save();
+
+      response.redirect("/stylists/" + stylistId);
+      return null;
+    });
+
+    //page for client creation format
+    get("/stylists/:id/clients/:client_id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Client client = Client.find(Integer.parseInt(request.params(":client_id")));
+      model.put("client", client);
+      model.put("template", "templates/client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //page for client creation format
+    get("/stylists/:id/clients/:client_id/edit", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Client client = Client.find(Integer.parseInt(request.params(":client_id")));
+      model.put("client", client);
+      model.put("template", "templates/client-edit.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //create new client object
+    post("/stylists/:id/clients/:client_id/delete", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int clientId = Integer.parseInt(request.queryParams("client_id"));
+      Client myClient = Client.find(clientId);
+      int stylistId = myClient.getStylistId();
+      myClient.delete();
+      response.redirect("/stylists/" + stylistId + "/clients");
+      return null;
+    });
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -45,7 +45,7 @@ public class AppTest extends FluentTest {
     assertThat(pageSource().contains("Stylist Stephen's clients:"));
   }
 
-  //view stylist edit pageSource
+  //view stylist edit page
   @Test public void viewStylistEditPage() {
     Stylist myStylist = new Stylist("Stephen");
     myStylist.save();
@@ -76,62 +76,62 @@ public class AppTest extends FluentTest {
     assertEquals(0, Stylist.all().size());
   }
 
-  // //add a client to an individual stylist
-  // @Test public void createClientForStylistAndViewList() {
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Create New Stylist"));
-  //   fill("#name").with("Stephen");
-  //   submit(".btn");
-  //   click("a", withText("Stephen"));
-  //   click("a", withText("Create client"));
-  //   fill("#name").with("Stacy");
-  //   submit(".btn");
-  //   assertThat(pageSource().contains("Stacy"));
-  // }
-  //
-  // //view individual client page
-  // @Test public void viewClientPage() {
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Create New Stylist"));
-  //   fill("#name").with("Stephen");
-  //   submit(".btn");
-  //   click("a", withText("Stephen"));
-  //   click("a", withText("Create client"));
-  //   fill("#name").with("Stacy");
-  //   submit(".btn");
-  //   click("a", withText("Stacy"));
-  //   assertThat(pageSource().contains("Client: Stacy"));
-  // }
-  //
-  // //edit a client name
-  // @Test public void editClientAndView() {
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Create New Stylist"));
-  //   fill("#name").with("Stephen");
-  //   submit(".btn");
-  //   click("a", withText("Stephen"));
-  //   click("a", withText("Create client"));
-  //   fill("#name").with("Stacy");
-  //   submit(".btn");
-  //   click("a", withText("Stacy"));
-  //   click("a", withText("Change client name"));
-  //   fill("#name").with("Shawn");
-  //   submit(".btn");
-  //   assertThat(pageSource().contains("Shawn"));
-  // }
-  //
-  // //delete a client
-  // @Test public void deleteAClientAnd() {
-  //   goTo("http://localhost:4567/");
-  //   Stylist myStylist = new Stylist("Stephen");
-  //   myStylist.save();
-  //   goTo("http://localhost:4567/stylists/");
-  //   click("a", withText("Stephen"));
-  //   click("a", withText("Create client"));
-  //   fill("#name").with("Stacy");
-  //   submit(".btn");
-  //   click("a", withText("Stacy"));
-  //   click("a", withText("Delete Client"));
-  //   assertEquals(0, myStylist.getClients().size());
-  // }
+  //add a client to an individual stylist
+  @Test public void createClientForStylistAndViewList() {
+    Stylist myStylist = new Stylist("Stephen");
+    myStylist.save();
+    String path = String.format("http://localhost:4567/stylists/%d/clients/new", myStylist.getId());
+    goTo(path);
+    fill("#name").with("Betsy");
+    submit(".btn");
+    assertThat(pageSource().contains("Besty"));
+  }
+
+  //view individual client page
+  @Test public void viewClientPage() {
+    Stylist myStylist = new Stylist("Stephen");
+    myStylist.save();
+    Client myClient = new Client("Betsy", myStylist.getId());
+    myClient.save();
+    String path = String.format("http://localhost:4567/stylists/%d/clients/%d", myClient.getStylistId(), myClient.getId());
+    goTo(path);
+    assertThat(pageSource().contains("Client: Betsy"));
+  }
+
+  //view client edit page
+  @Test public void viewClientEditPage() {
+    Stylist myStylist = new Stylist("Stephen");
+    myStylist.save();
+    Client myClient = new Client("Betsy", myStylist.getId());
+    myClient.save();
+    String path = String.format("http://localhost:4567/stylists/%d/clients/%d/edit", myClient.getStylistId(), myClient.getId());
+    goTo(path);
+    assertThat(pageSource().contains("Change Betsy's name"));
+  }
+
+  //edit a client name
+  @Test public void editClientAndView() {
+    Stylist myStylist = new Stylist("Stephen");
+    myStylist.save();
+    Client myClient = new Client("Betsy", myStylist.getId());
+    myClient.save();
+    String path = String.format("http://localhost:4567/stylists/%d/clients/%d/edit", myClient.getStylistId(), myClient.getId());
+    goTo(path);
+    fill("#name").with("Bob");
+    submit(".btn");
+    assertThat(pageSource().contains("Bob"));
+  }
+
+  //delete a client
+  @Test public void deleteAClientAnd() {
+    Stylist myStylist = new Stylist("Stephen");
+    myStylist.save();
+    Client myClient = new Client("Betsy", myStylist.getId());
+    myClient.save();
+    String path = String.format("http://localhost:4567/stylists/%d/clients/%d/edit", myClient.getStylistId(), myClient.getId());
+    goTo(path);
+    click("button", withText("Delete this client"));
+    assertThat(pageSource().contains("Bob"));
+    assertEquals(0, myStylist.getClients().size());
+  }
 }
