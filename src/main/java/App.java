543a4292dd2cbc70cc4f4120d5fffb2f36a67d60
Daniel Wulff <danielwulff.ee@gmail.com>
2016-05-09 14:27:46 -0700

    created interface for index, adding a stylists and viewing list of stylists. Added tests for clients of stylists viewing and client creation, but tests not passing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -6,6 +7,38 @@ import static spark.Spark.*;
 public class App {
 
   public static void main (String[] args){
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
+    //index
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //list all stylists
+    get("/stylists", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //page for stylist creation form
+    get("/stylists/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/stylists-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //create new stylist object
+    post("/stylists", (request, response) -> {
+      String name = request.queryParams("name");
+      Stylist newStylist = new Stylist(name);
+      newStylist.save();
+      response.redirect("/stylists");
+      return null;
+    });
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,12 @@
 import org.fluentlenium.adapter.FluentTest;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.sql2o.*;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -16,9 +19,40 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("");
-  // }
+  @Rule
+    public DatabaseRule database = new DatabaseRule();
 
+  @Test public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Hair Salon");
+  }
+
+  @Test public void createStylistAndViewTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Create New Stylist"));
+    fill("#name").with("Stephen");
+    submit(".btn");
+    assertThat(pageSource().contains("Stephen"));
+  }
+
+  @Test public void viewStylistPage() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Create New Stylist"));
+    fill("#name").with("Stephen");
+    submit(".btn");
+    click("a", withText("Stephen"));
+    assertThat(pageSource().contains("Stylist Stephen's clients:"));
+  }
+
+  @Test public void createClientForStylistAndView() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Create New Stylist"));
+    fill("#name").with("Stephen");
+    submit(".btn");
+    click("a", withText("Stephen"));
+    click("a", withText("Create client"));
+    fill("#name").with("Stacy");
+    submit(".btn");
+    assertThat(pageSource().contains("Stacy"));
+  }
 }
